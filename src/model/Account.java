f9d2314ee954d57a90a70f78cbe619e05b60efe0
snehasmitapal20 <snehasmita.pal20@gmail.com>
2026-03-25 09:00:57 +0530

    Added Account base class with deposit and withdraw methods

---
+++ src/model/Account.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Account {
+    protected int accountNumber;
+    protected double balance;
+
+    public Account(int accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient balance");
+        } else {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
